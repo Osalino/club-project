6694a61f3ce9b3a787063897f49c8c6f4810d53d
osalino <osazemen.odeh@gmail.com>
2025-03-02 23:48:51 +0000

    Initial commit

---
+++ src/Club.java
@@ -0,0 +1,109 @@
+import java.util.*;
+
+/**
+ * Store details of club memberships.
+ *
+ * @author A. Razak
+ * @version version 1 - 23/02/2025
+        */
+    public class Club {
+    // Define any necessary fields here ...
+    private ArrayList<Member> MembersList;
+    private Member month;
+
+        /**
+     * Constructor for objects of class Club
+     */
+    public Club() {
+        // Initialise any fields here ...
+        MembersList = new ArrayList<Member>();
+
+    }
+    /**
+     * Add a new member to the club's list of members.
+     * @param newMember The member object to be added.
+     */
+
+    public void join(Member newMember) {
+
+        MembersList.add(newMember);
+        System.out.println(newMember.getName() + " joined.");
+//        System.out.println("##############################\n");
+
+    }
+
+    /**
+     * @return The number of members (Membership objects) in
+     *         the club.
+     */
+    public int numberOfMembers() {
+        return MembersList.size();
+    }
+
+    public int joinedInMonth(int month) {
+        if(month < 1 || month > 12){
+            System.out.println("Invalid month number");
+            return 0;
+        }
+        int count = 0;
+
+        for(Member member : MembersList) {
+            if(member.getMonth() == month) {
+                    count++;
+            }
+        }
+//        Member member = MembersList.get(month);
+//        if(member == null){
+//            System.out.println("Invalid month number");
+//        }
+        return count;
+    }
+
+    public ArrayList<Member> removedMembers (int month, int year){
+        if (month < 1 || month > 12){
+            System.out.println("Invalid month number");
+            return null;
+        } else {
+            Iterator<Member> iterator = MembersList.iterator();
+            while(iterator.hasNext()){
+                Member member = iterator.next();
+                if(member.getMonth() == month && member.getYear() == year){
+                    iterator.remove();
+                    Member.removedMembers();
+                    System.out.printf("The memeber %s, has been removed successfully.%n", member.getName());
+                }
+            }
+        }
+        return MembersList;
+    }
+
+    public void printDetails() {
+        Iterator<Member> iterator = MembersList.iterator();
+        while(iterator.hasNext()){
+            Member member = iterator.next();
+            System.out.printf("\n%s joined, the club in %d %d  ",member.getName(),member.getMonth(),member.getYear());
+        }
+    }
+
+    public void manualPrint() {
+        System.out.println();
+//        Iterator<Member> iterator = MembersList.iterator();
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter Your name: ");
+        String user = myObj.nextLine();
+        System.out.println("Enter Your month: ");
+        int month2 = myObj.nextInt();
+        System.out.println("Enter Your year: ");
+        int year2 = myObj.nextInt();
+
+        Member newMember = new Member(user, month2, year2);
+        MembersList.add(newMember);
+    }
+
+
+
+
+    }
+
+
+
---
+++ src/TestClub.java
@@ -0,0 +1,63 @@
+/**
+ * Provide a demonstration of the Club and Member
+ * classes.
+ * 
+ * @author A.Razak 
+ * @version version 1 - 23/02/2025
+ */
+public class TestClub
+{
+	
+    // instance variables - replace the example below with your own
+    
+    /**
+     * Constructor for objects of class TestClub
+     */
+    public TestClub()
+    {
+      
+   }
+
+    public static void main(String args[]){
+    
+    /**
+     * Add some members to the club, and then
+     * show how many there are.
+     * Further example calls could be added if more functionality
+     * is added to the Club class.
+     */
+       	Club myClub = new Club();
+
+           System.out.println("The club has " +
+                  myClub.numberOfMembers() +
+                  " members.");
+        System.out.println("\n##############################");
+
+        myClub.join(new Member("John", 2, 2017));
+        myClub.join(new Member("Abdi", 4, 2017));
+        myClub.join(new Member("Michael", 7, 2018));
+
+
+
+        System.out.println("\n##############################");
+        myClub.removedMembers(2,2017);
+        myClub.joinedInMonth(1);
+
+        System.out.println("The club currently has " +
+                             myClub.numberOfMembers() +
+                " members.");
+
+        System.out.println("\n##############################");
+
+        myClub.printDetails();
+
+        System.out.println("\n############# - Manual Input - #################");
+
+        myClub.manualPrint();
+
+        myClub.printDetails();
+
+    }
+    
+    
+}
